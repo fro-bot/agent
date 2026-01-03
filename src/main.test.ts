b1bbc24fe7969563d64176f7972dd789a7aa3645
Marcus R. Brown <git@mrbro.dev>
2026-01-03 00:06:47 -0700

    test: simplify integration test

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,15 +1,13 @@
-import * as cp from 'node:child_process'
-import * as path from 'node:path'
-import * as process from 'node:process'
+import {exec} from 'node:child_process'
+import process from 'node:process'
+import {promisify} from 'node:util'
 import {expect, it} from 'vitest'
 
-// shows how the runner will run a javascript action with env / stdout protocol
-it('runs', () => {
-  process.env.INPUT_MILLISECONDS = '500'
-  const np = process.execPath
-  const ip = path.join(path.dirname(new URL(import.meta.url).pathname), '..', 'dist', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env,
-  }
-  expect(cp.execFileSync(np, [ip], options).toString()).toMatch(/::set-output name=time::/)
+const execAsync = promisify(exec)
+
+it('runs successfully with valid inputs', async () => {
+  const {stderr} = await execAsync(`node dist/main.js`, {
+    env: {...process.env, INPUT_MILLISECONDS: '500', GITHUB_OUTPUT: '/dev/null'},
+  })
+  expect(stderr).toBe('')
 })
